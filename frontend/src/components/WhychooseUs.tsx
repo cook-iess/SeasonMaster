28ed38c4e3a570ce829244cb02d6a0660ff7d4c0
cook-iess <ekramsiraj32@gmail.com>
2025-10-16 09:05:29 +0300

    why choose us section responsiveness done

--- frontend/src/components/WhychooseUs.tsx
+++ frontend/src/components/WhychooseUs.tsx
@@ -5,16 +5,16 @@ export default function WhyChooseSection({ data }: { data: WhyChooseData }) {
     const { title, description, Image: image, why_choose_lists: cards } = data;
 
     return (
-        <section className="px-28 py-18">
+        <section className="md:px-28 px-4 md:pt-18 md:pb-18 pt-8 pb-10">
             <div className="max-w-6xl mx-auto">
                 <div className="flex flex-col md:flex-row gap-10">
                     {/* Left */}
                     <div className="md:basis-[640px] md:max-w-[640px] space-y-6 flex-shrink-0">
                         <div className="space-y-2 mb-6">
-                            <h2 className="text-3xl md:text-[44px] font-bold text-[#1C3960]">{title}</h2>
-                            <p className="text-lg text-[#333333]">{description}</p>
+                            <h2 className="text-2xl md:text-[44px] font-bold text-[#1C3960]">{title}</h2>
+                            <p className="text-sm md:text-lg text-[#333333]">{description}</p>
                         </div>
-                        <div className="relative h-64 md:h-96 w-full mt-6">
+                        <div className="relative h-96 w-full mt-6">
                             <Image
                                 src={`${process.env.STRAPI_URL}${image.url}`}
                                 alt={image.alternativeText || 'Why Choose Season Master'}
@@ -27,12 +27,12 @@ export default function WhyChooseSection({ data }: { data: WhyChooseData }) {
 
                     {/* Right */}
                     <div className="md:basis-[428px] md:max-w-[428px] flex-shrink-0 flex gap-6">
-                        <div className="w-[3px] bg-black self-stretch flex-shrink-0"></div>
-                        <div className="flex flex-col space-y-6">
+                        <div className="w-[3px] bg-black self-stretch flex-shrink-0 hidden md:block"></div>
+                        <div className="flex flex-col space-y-6 md:w-fit w-full">
                             {cards.map((card) => (
                                 <div
                                     key={card.id}
-                                    className="space-y-2 p-4 rounded-lg bg-[#26C7BE] text-white"
+                                    className="space-y-2 p-4 rounded-lg bg-[#26C7BE] hover:bg-[#26C7BE]/90 duration-300 text-white"
                                 >
                                     <h3 className="text-2xl font-bold">{card.cardTitle}</h3>
                                     <p className="text-base font-light">{card.cardDescription}</p>
