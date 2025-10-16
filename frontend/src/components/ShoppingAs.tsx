d1835b895329cadce1da86782de39607e7bce38f
cook-iess <ekramsiraj32@gmail.com>
2025-10-16 09:04:08 +0300

    shopping as section responsive done

--- frontend/src/components/ShoppingAs.tsx
+++ frontend/src/components/ShoppingAs.tsx
@@ -24,15 +24,15 @@ export default function ShoppingAs({ data }: { data: ShoppingAsData }) {
     const [left, right] = items;
 
     return (
-        <section className="py-18 px-10 max-w-[1512px] mx-auto">
-            <div className="">
+        <section className="py-18 md:px-10 px-4 pt-6 md:pt-18 md:pb-18 pb-14 max-w-[1512px] mx-auto">
+            <div className="space-y-8">
                 <div className="mb-10 space-y-2">
-                    <h2 className="text-3xl md:text-[44px] font-bold text-[#1C3960]">{title}</h2>
-                    <p className="text-lg text-[#333333] max-w-3xl">{description}</p>
+                    <h2 className="md:text-3xl text-2xl md:text-[44px] font-bold text-[#1C3960]">{title}</h2>
+                    <p className="md:text-lg text-sm text-[#333333] max-w-3xl">{description}</p>
                 </div>
 
-                {/* Two Cards Side-by-Side */}
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-x-4">
+                {/* Two Cards */}
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-x-4 md:gap-y-0 gap-y-4">
                     {[left, right].map((item) => (
                         <div key={item.id} className="relative group">
                             <div className="relative w-full h-[50vh] md:h-[60vh] lg:h-[600px] overflow-hidden rounded-2xl">
@@ -46,16 +46,16 @@ export default function ShoppingAs({ data }: { data: ShoppingAsData }) {
 
                                 {/* Content at bottom */}
                                 <div className="absolute bottom-0 left-0 right-0 p-6 text-white">
-                                    <span className="inline-block rounded-sm text-base font-bold bg-[#19857F] p-2 gap-x-8">
+                                    <span className="inline-block rounded text-xs md:text-base md:font-bold font-semibold  bg-[#19857F] hover:bg-[#20A69E] duration-300 px-2 md:py-2 py-1 gap-x-8">
                                         {item.who}
                                     </span>
                                     <div className='space-y-4 my-6'>
-                                        <h3 className="text-[44px] font-bold mb-2">{item.why}</h3>
-                                        <p className="mb-4 text-gray-200">{item.description}</p>
+                                        <h3 className="lg:text-[44px] sm:text-[32] text-[24px] font-bold mb-2">{item.why}</h3>
+                                        <p className="text-base md:text-lg">{item.description}</p>
                                     </div>
-                                    <button className="flex items-center gap-2 font-semibold group/button bg-[#0284A3] px-10 py-4 rounded-lg">
+                                    <button className="flex items-center justify-center gap-2 font-semibold group/button bg-[#0284A3] hover:bg-[#0284A3]/80 duration-300 px-10 py-4 rounded-lg w-full">
                                         {item.buttonText}
-                                        <span className="group-hover/button:translate-x-1 transition-transform">→</span>
+                                        <span className="group-hover/button:translate-x-1 transition-transform text-sm md:text-lg">→</span>
                                     </button>
                                 </div>
                             </div>
