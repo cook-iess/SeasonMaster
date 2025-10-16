bd24b0a6095e0c70df58aa82b8f0128fd1477bd7
cook-iess <ekramsiraj32@gmail.com>
2025-10-16 09:09:06 +0300

    testimonials section responsive

--- frontend/src/components/Testimonials.tsx
+++ frontend/src/components/Testimonials.tsx
@@ -35,23 +35,78 @@ export default function TestimonialsSection({
     const socialIconUrl = STRAPI_URL + rating.socialIcon.url;
     const starsImageUrl = STRAPI_URL + rating.stars.url;
 
+    const GoogleRatingBlock = ({
+        rating,
+        size,
+    }: {
+        rating: GoogleRatingData;
+        size: number;
+    }) => {
+        const socialIconUrl = STRAPI_URL + rating.socialIcon.url;
+        const starsImageUrl = STRAPI_URL + rating.stars.url;
+
+        return (
+            <div className="flex flex-row items-center mx-auto gap-x-2 max-w-max">
+                <Image
+                    src={socialIconUrl}
+                    alt="Google"
+                    width={size}
+                    height={size}
+                    className={`rounded-full w-[${size}px] h-[${size}px] object-contain`}
+                />
+                <div>
+                    <div className={`${jakarta.className} md:text-[11.28px] text-[6.92px] font-semibold`}>
+                        {rating.socialName}
+                    </div>
+                    <div className={`${jakarta.className} md:text-[11.28px] text-[6.92px]`}>
+                        {rating.ratingDesc}
+                    </div>
+                    <div>
+                        <Image
+                            src={starsImageUrl}
+                            alt="Google rating stars"
+                            width={55}
+                            height={8}
+                            className="object-contain"
+                        />
+                    </div>
+                </div>
+                <div className="text-sm md:text-base">{rating.ratedText} <span className="md:hidden">based on <u>500 reviews.</u></span></div>
+            </div>
+        );
+    };
+
+
     return (
-        <section className="px-12 py-18">
-            <div className="mx-auto  space-y-8">
-                <h2 className="text-[44px] font-bold text-[#1C3960] text-center mb-8 md:mb-12">{Header}</h2>
+        <section className="md:px-12 md:py-18 px-4 py-8">
+            <div className="mx-auto space-y-8">
+                <div className="space-y-4 md:space-y-0">
+                    <h2 className="lg:text-[44px] sm:text-3xl text-2xl font-bold text-[#1C3960] md:text-center mb-8 md:mb-12">{Header}</h2>
+                    <div className="space-x-2 md:hidden">
+                        <span className={`${jakarta.className} inline-block p-2 rounded text-sm font-bold bg-[#0284A3] text-white`}>
+                            Homeowners
+                        </span>
+                        <span className={`${jakarta.className} inline-block p-2 text-sm font-bold bg-[#F5F5F5] text-[#1C3960]`}>
+                            Trade Installers
+                        </span>
+                    </div>
+                </div>
 
                 <div className="space-y-6">
+                    <div className="md:hidden">
+                        <GoogleRatingBlock rating={rating} size={29} />
+                    </div>
                     {/* ShadCN Carousel */}
                     <Carousel
                         opts={{
                             align: "start",
                             slidesToScroll: 1,
                         }}
-                        className="w-full max-w-[70%] mx-auto"
+                        className="w-full md:max-w-[70%] max-w-[80%] mx-auto"
                     >
                         <CarouselContent className="-ml-4">
                             {testimonials.map((item) => (
-                                <CarouselItem key={item.id} className="pl-4 max-w-80">
+                                <CarouselItem key={item.id} className="pl-4 md:max-w-80 max-w-[95%] sm:max-w-[85%]">
                                     <div className="p-4 bg-[#EAFCFB] rounded-md shadow-sm h-full space-y-2.5">
                                         <div className="flex mb-3">
                                             <Image
@@ -62,13 +117,13 @@ export default function TestimonialsSection({
                                                 className="object-contain"
                                             />
                                         </div>
-                                        <p className="text-[#0A1422] font-light text-base">{item.review}</p>
+                                        <p className="text-[#0A1422] font-light md:text-base text-sm">{item.review}</p>
                                         <div className="border-t-[0.5px] border-[#1C3960]/20"></div>
-                                        <div className="flex justify-between">
-                                            <p className="font-bold text-sm text-[#152B48]">
+                                        <div className="flex justify-between md:text-sm text-xs">
+                                            <p className="font-bold  text-[#152B48]">
                                                 {item.reviewer}
                                             </p>
-                                            <p className="text-sm text-[#0A1422]">
+                                            <p className="text-[#0A1422]">
                                                 {formatDate(item.date)}
                                             </p>
                                         </div>
@@ -76,36 +131,13 @@ export default function TestimonialsSection({
                                 </CarouselItem>
                             ))}
                         </CarouselContent>
-                        <CarouselPrevious className="-left-16" />
-                        <CarouselNext className="-right-16" />
+                        <CarouselPrevious className="md:-left-16 -left-11" />
+                        <CarouselNext className="md:-right-16 -right-11" />
                     </Carousel>
 
                     {/* Rating */}
-                    <div className="flex flex-col sm:flex-row items-center justify-center gap-x-3 max-w-max mx-auto mt-10">
-                        <Image
-                            src={socialIconUrl}
-                            alt="Google"
-                            width={47.36}
-                            height={47.36}
-                            className="rounded-full"
-                        />
-                        <div>
-                            <div className={`${jakarta.className} text-[11.28px] font-semibold`}>
-                                {rating.socialName}
-                            </div>
-                            <div className={`${jakarta.className} text-[11.28px]`}>
-                                {rating.ratingDesc}
-                            </div>
-                            <div className="">
-                                <Image
-                                    src={starsImageUrl}
-                                    alt="Google rating stars"
-                                    width={90}
-                                    height={18}
-                                />
-                            </div>
-                        </div>
-                        <div className="text-base">{rating.ratedText}</div>
+                    <div className="hidden md:block">
+                        <GoogleRatingBlock rating={rating} size={47} />
                     </div>
                 </div>
             </div>
