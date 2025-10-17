0fad644ab00da713a1418e61cedc4c00e37e90de
kuki44 <ekramsiraj32@gmail.com>
2025-10-17 07:42:08 +0300

    installed awss3 and also another fix for the cookie thing because admin login via the new host is still not working

--- backend/config/server.ts
+++ backend/config/server.ts
@@ -1,9 +1,9 @@
-module.exports = ({ env }) => ({
+export default ({ env }) => ({
   url: env('PUBLIC_URL', 'https://seasonmaster-1.onrender.com'),
   proxy: true, 
 
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 10000),
+  port: env.int('PORT', 10000), 
   
   app: {
     keys: env.array('APP_KEYS'),
