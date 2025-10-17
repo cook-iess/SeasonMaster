f7240868c9e055ce9e75ceb6c28f6fd1a4f0cf33
kuki44 <ekramsiraj32@gmail.com>
2025-10-17 06:17:33 +0300

    Fix Admin Login (Secure Cookie Error

--- backend/config/server.ts
+++ backend/config/server.ts
@@ -1,6 +1,10 @@
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
+  url: env('PUBLIC_URL', 'https://seasonmaster-1.onrender.com'),
+  proxy: true, 
+
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 10000),
+  
   app: {
     keys: env.array('APP_KEYS'),
   },
