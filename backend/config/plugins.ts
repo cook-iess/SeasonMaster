7ac6d7446d38127de56ea2d54ff217f2b81a07e9
cook-iess <ekramsiraj32@gmail.com>
2025-10-25 10:00:14 +0300

    S3 configuration options passed now are wrapped inside 's3Options:{}'

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   upload: {
     config: {
       provider: 'aws-s3',
-      providerOptions: {
+      s3Options: { // 👈 wrap here
         accessKeyId: env('AWS_ACCESS_KEY_ID'),
         secretAccessKey: env('AWS_ACCESS_SECRET'),
         region: env('AWS_REGION'),
