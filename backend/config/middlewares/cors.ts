f891aa50dc66b6e2e75f759bed59cb930135fac2
cook-iess <ekramsiraj32@gmail.com>
2025-10-25 10:39:23 +0300

    fix #3

--- backend/config/middlewares/cors.ts
+++
@@ -1,9 +0,0 @@
-export default ({ env }) => ({
-  config: {
-    headers: '*',
-    origin: [
-      env('PUBLIC_URL', 'https://seasonmaster-1.onrender.com').trim(),
-      env('FRONT_URL', 'https://season-master-um9e.vercel.app').trim(),
-    ],
-  },
-});
--- backend/config/middlewares/session.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  config: {
-    cookie: {
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-    },
-  },
-};
